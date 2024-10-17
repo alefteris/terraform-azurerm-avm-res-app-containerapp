7a0a268373e0db1224bb64bf2de61575113f96e6
Thanos Lefteris <alefteris@gmail.com>
2024-10-17 14:48:06 +0300

    Add azure/modtm to required_providers

--- terraform.tf
+++ terraform.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.85, < 4.0"
     }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
